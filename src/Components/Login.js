6db48d63834fb409afeb2a2c68a4303aee504ceb
pattersondev <pattersonrsam@gmail.com>
2023-11-04 15:46:54 -0400

    small tweaks

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -19,59 +19,26 @@ function Login() {
     },
   }))(Button);
 
-  const svgRef = useRef(null); // Reference to the SVG element
-  const textRef = useRef(null); // Reference to the textPath element
-
-  const updateText = () => {
-    //const svg = svgRef.current;
-    const textPath = textRef.current;
-
-    // Calculate the new startOffset based on text length
-    const textLength = textPath.textContent.length;
-    const maxTextLength = 8;
-    const baseStartOffset = 50;
-
-    let startOffset;
-    if (textLength <= 4) {
-      startOffset = 50;
-    } else {
-      startOffset = baseStartOffset - (textLength - maxTextLength) * 0.2;
-    }
-
-    startOffset = Math.max(0, Math.min(100, startOffset));
-    textPath.setAttribute("startOffset", `${startOffset}%`);
-
-    // Calculate the new font size based on text length
-    const fontSize = Math.min(24, 240 / (textLength * 1.3));
-
-    // Update the font size
-    textPath.setAttribute("font-size", fontSize);
-  };
-
-  useEffect(() => {
-    updateText(); // Call the updateText function after the component mounts
-  }, []);
-
   return (
     <div>
       <div className="login">
         <IconButton href={loginUrl} className="button-container">
           <div>
             <div id="svg-container">
-              <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" version="1.1" ref={svgRef}>
-                <circle cx="120" cy="120" r="112" fill="#ffd64a" stroke="#282725" strokeWidth="4" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" version="1.1">
+                <circle cx="120" cy="120" r="112" fill="#fbd847" stroke="#282725" strokeWidth="4" />
                 <ellipse cx="90" cy="84" rx="12" ry="30" fill="#282725" />
                 <ellipse cx="150" cy="84" rx="12" ry="30" fill="#282725" />
                 <path id="smilePath" d="M36 162 Q120 216, 204 162" fill="none" />
                 <text
                   id="text"
-                  fontSize="24"
+                  fontSize="13"
                   fontFamily="Inter Tight"
                   fill="black"
                   textAnchor="middle"
                   letterSpacing="2.4"
                 >
-                  <textPath href="#smilePath" startOffset="50%" ref={textRef}>
+                  <textPath href="#smilePath" startOffset="50%">
                     Login With Spotify
                   </textPath>
                 </text>
