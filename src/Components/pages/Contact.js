572fa3147921286378f242748563c91956674203
jackcameron <jackmcameron2@gmail.com>
2023-11-10 00:45:24 -0500

    Contact Page Styled v1

--- src/Components/pages/Contact.js
+++ src/Components/pages/Contact.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import "./About.css";
+//import "./About.css";
+import "./ee.css";
+
 
 function Contact() {
     return (
-        <div class="center">
-                <text style={{ fontSize: 36, marginTop: '6rem' }}>CONTACT </text>
+        <div className="center">
+          <div>
+            <h1 style={{ fontSize: 50, marginTop: '6rem' }}>contact happify.</h1>
+          </div>
+          <div>
+            <h2 style={{ fontSize: 18, marginTop: '3rem' }}>please refer to our primary email for all questions and inquiries.</h2>
+          </div>
+          <div className ="easterEggContainer">
+            <h2 className="mainHeading"style={{ fontSize: 18, marginTop: '3rem' }}>happifybiz@gmail.com</h2>
+            <p className="hiddenText">hi justin</p>
+          </div>
         </div>
-    );
+      );
 }
 
 export default Contact;
