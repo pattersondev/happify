42684ae881fd526264e019d538d76ceeb8cd70a4
pattersondev <pattersonrsam@gmail.com>
2023-11-13 18:52:51 -0500

    fixed typos and redundancy

--- src/Components/pages/Privacy.js
+++ src/Components/pages/Privacy.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
 function Privacy() {
-    return (
-      <div className="center">
-        <div>
-          <h1 style={{ fontSize: 50, marginTop: '6rem' }}>happify private policy.</h1>
-        </div>
-        <div>
-          <h2 style={{ fontSize: 18, marginTop: '3rem' }}>happify does not collect or store any data that is used in generation of a user report, or share to third parties.</h2>
-        </div>
-        <div>
-          <h2 style={{ fontSize: 18, marginTop: '3rem' }}>it was created and developed by jack cameron and sam patterson in November of 2023.</h2>
-        </div>
+  return (
+    <div className="center">
+      <div>
+        <h1 style={{ fontSize: 50, marginTop: '6rem' }}>happify privacy policy.</h1>
       </div>
-    );
-  }
+      <div>
+        <h2 style={{ fontSize: 18, marginTop: '3rem' }}>happify does not collect or store any data that is used in generation of a user report, or shared to third parties.</h2>
+      </div>
+    </div>
+  );
+}
 
 export default Privacy;
