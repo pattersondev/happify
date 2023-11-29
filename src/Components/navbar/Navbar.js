9f3c9f0da04950748153122a538439544e8534bd
pattersondev <pattersonrsam@gmail.com>
2023-11-28 20:47:58 -0500

    WHAT IS HAPPENING

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const NavBar = () => {
-    const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
-            <button className="menu-toggle" onClick={toggleMenu}>
-                ☰
-            </button>
-            <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
-                <Link to="/" className="nav-link" onClick={toggleMenu}>
-                    Home
-                </Link>
-                <Link to="/about" className="nav-link" onClick={toggleMenu}>
-                    About
-                </Link>
-                <Link to="/contact" className="nav-link" onClick={toggleMenu}>
-                    Contact
-                </Link>
-                <Link to="/privacy" className="nav-link" onClick={toggleMenu}>
-                    Privacy
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/" className="nav-link" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link to="/about" className="nav-link" onClick={toggleMenu}>
+          About
+        </Link>
+        <Link to="/contact" className="nav-link" onClick={toggleMenu}>
+          Contact
+        </Link>
+        <Link to="/privacy" className="nav-link" onClick={toggleMenu}>
+          Privacy
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
